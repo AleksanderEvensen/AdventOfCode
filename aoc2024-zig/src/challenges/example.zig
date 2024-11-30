3a06c8f539be4cd0241a9447ddee4ca60af544b7
Aleksander Evensen <ahse03@gmail.com>
2024-11-30 23:47:07 +0100

    Prepared for AOC2024 in Zig

---
+++ aoc2024-zig/src/challenges/example.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+const File = std.fs.File;
+const expect = std.testing.expect;
+const equal = std.mem.eql;
+
+pub fn solve(inputFile: []const u8) !void {
+    const file = try std.fs.cwd().openFile(inputFile, .{});
+    defer file.close();
+
+    var bufferReader = std.io.bufferedReader(file.reader());
+    var inputReader = bufferReader.reader();
+
+    var buffer: [1024]u8 = undefined;
+    while (try inputReader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
+        std.debug.print("line: {}\n", .{line});
+    }
+}
---
+++ aoc2024-zig/src/main.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+const day1 = @import("./challenges/day1.zig");
+const day2 = @import("./challenges/day2.zig");
+const day3 = @import("./challenges//day3.zig");
+
+const Challenge = struct {
+    inputFile: []const u8,
+    solveFn: *const fn ([]const u8) anyerror!void,
+};
+
+pub fn main() !void {
+
+    //
+
+    const challenges = [_]Challenge{
+        Challenge{ .inputFile = "./inputs/day1.txt", .solveFn = &day1.solve },
+        Challenge{ .inputFile = "./inputs/day2.txt", .solveFn = &day2.solve },
+        Challenge{ .inputFile = "./inputs/day3.txt", .solveFn = &day3.solve },
+    };
+
+    for (challenges, 1..) |challenge, i| {
+        std.debug.print("Running solver for day{}\n", .{i});
+        try challenge.solveFn(challenge.inputFile);
+    }
+    std.debug.print("All done :)\n", .{});
+}
