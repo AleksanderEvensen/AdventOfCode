1d4a7dca56a560c86ad75dcfd7fcb43154ea8e17
Aleksander Evensen <ahse03@gmail.com>
2024-12-01 01:43:16 +0100

    Use @embedFile for inputs, and time meassure the functions

--- aoc2024-zig/src/challenges/example.zig
+++ aoc2024-zig/src/challenges/example.zig
@@ -1,17 +1,5 @@
 const std = @import("std");
-const File = std.fs.File;
-const expect = std.testing.expect;
-const equal = std.mem.eql;
 
-pub fn solve(inputFile: []const u8) !void {
-    const file = try std.fs.cwd().openFile(inputFile, .{});
-    defer file.close();
-
-    var bufferReader = std.io.bufferedReader(file.reader());
-    var inputReader = bufferReader.reader();
-
-    var buffer: [1024]u8 = undefined;
-    while (try inputReader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
-        std.debug.print("line: {}\n", .{line});
-    }
+pub fn solve(input: []const u8) !void {
+    std.debug.print("Input: \n\n###\n{s}\n###\n\n", .{input});
 }
--- aoc2024-zig/src/main.zig
+++ aoc2024-zig/src/main.zig
@@ -2,18 +2,41 @@ const std = @import("std");
 const day1 = @import("./challenges/day1.zig");
 
 const Challenge = struct {
-    inputFile: []const u8,
+    input: []const u8,
     solveFn: *const fn ([]const u8) anyerror!void,
 };
 
 pub fn main() !void {
     const challenges = [_]Challenge{
-        Challenge{ .inputFile = "./inputs/day1.txt", .solveFn = &day1.solve },
+        Challenge{ .input = @embedFile("./inputs/day1.txt"), .solveFn = &day1.solve },
     };
 
-    for (challenges, 1..) |challenge, i| {
-        std.debug.print("Running solver for day{}\n", .{i});
-        try challenge.solveFn(challenge.inputFile);
+    for (challenges, 1..) |challenge, day| {
+        std.debug.print("Running solver for day{}\n", .{day});
+
+        const start = std.time.microTimestamp();
+        try challenge.solveFn(challenge.input);
+        const timeDiff: i64 = std.time.microTimestamp() - start;
+
+        std.debug.print("Done solving day{}, it took {s}\n", .{ day, try formatDurration(timeDiff) });
     }
     std.debug.print("All done :)\n", .{});
+}
+
+fn formatDurration(nanoseconds: i64) anyerror![]const u8 {
+    const fNanoseconds: f64 = @floatFromInt(nanoseconds);
+
+    const unit = switch (nanoseconds) {
+        0...1000 => "ns",
+        1001...100_000 => "ms",
+        else => "s",
+    };
+    const unitValue = switch (nanoseconds) {
+        0...1000 => fNanoseconds,
+        1001...100_000 => fNanoseconds / 1_000,
+        else => fNanoseconds / 1_000_000,
+    };
+
+    var buffer: [100]u8 = undefined;
+    return try std.fmt.bufPrint(&buffer, "{d}{s}", .{ unitValue, unit });
 }
