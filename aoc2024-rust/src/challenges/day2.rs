fb25d895a802f17e139f5f98817c33dc7536111a
Aleksander Evensen <ahse03@gmail.com>
2024-12-03 00:11:03 +0100

    O(n) solution rust 2024 Day 2

--- aoc2024-rust/src/challenges/day2.rs
+++ aoc2024-rust/src/challenges/day2.rs
@@ -1,37 +1,21 @@
 use std::ops::Mul;
 
 pub fn solve(input: String) {
-    
     let reports: Vec<Vec<isize>> = input.lines().map(|line|{
         line.split_whitespace().filter_map(|v|v.parse().ok()).collect()
     }).collect();
 
-    let part_1 = reports.iter().filter(|report| {
+    let (part_1, part_2) = reports.iter().fold((0,0), |acc, report| {
         let sign = (report[1] - report[0]).signum() | 1;
-        report
+        let errors = report
             .windows(2)
-            .all(|pair| (1..=3).contains(&(pair[1]-pair[0]).mul(sign)))
-    }).count();
+            .filter(|pair| !(1..=3).contains(&(pair[1]-pair[0]).mul(sign)))
+            .count();
 
-    let part_2 = reports.iter().filter(|report| {
         
-        (0..report.len()).any(|to_skip| {
-            let skipped_report = report.iter().enumerate()
-                .filter(|(i,_)| *i != to_skip)
-                .map(|(_,v)|v)
-                .collect::<Vec<&isize>>();
-            
-            let sign = (skipped_report[1] - skipped_report[0]).signum() | 1;
-
-            skipped_report
-                .windows(2)
-                .all(|pair| (1..=3).contains(&(pair[1]-pair[0]).mul(sign)))
-        })
-
-    }).count();
+        (acc.0 + (errors == 0) as isize, acc.1 + (errors < 2) as isize)
+    });
 
     println!("Part 1: {}", part_1);
     println!("Part 2: {}", part_2);
-
-
 }
