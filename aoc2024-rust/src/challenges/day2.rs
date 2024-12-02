229813d12af98bd736651d9792da8fe72201b27b
Aleksander Evensen <ahse03@gmail.com>
2024-12-02 14:21:58 +0100

    2024-rust: Day 2 optimized

--- aoc2024-rust/src/challenges/day2.rs
+++ aoc2024-rust/src/challenges/day2.rs
@@ -1,32 +1,37 @@
-pub fn solve(input: String) {
+use std::ops::Mul;
 
-    let part_1 = input.lines().filter(|line| {
-        let nums: Vec<isize> = line.split_whitespace().filter_map(|v| v.parse().ok()).collect();
-        is_report_valid(&nums)
+pub fn solve(input: String) {
+    
+    let reports: Vec<Vec<isize>> = input.lines().map(|line|{
+        line.split_whitespace().filter_map(|v|v.parse().ok()).collect()
+    }).collect();
+
+    let part_1 = reports.iter().filter(|report| {
+        let sign = (report[1] - report[0]).signum() | 1;
+        report
+            .windows(2)
+            .all(|pair| (1..=3).contains(&(pair[1]-pair[0]).mul(sign)))
     }).count();
 
-    let part_2 = input.lines().filter(|line| {
-        let nums: Vec<isize> = line.split_whitespace().filter_map(|v|v.parse().ok()).collect();
+    let part_2 = reports.iter().filter(|report| {
         
-        (0..nums.len()).any(|to_skip| { 
-            let mut report = nums.clone();
-            report.remove(to_skip);
-            is_report_valid(&report)
+        (0..report.len()).any(|to_skip| {
+            let skipped_report = report.iter().enumerate()
+                .filter(|(i,_)| *i != to_skip)
+                .map(|(_,v)|v)
+                .collect::<Vec<&isize>>();
+            
+            let sign = (skipped_report[1] - skipped_report[0]).signum() | 1;
+
+            skipped_report
+                .windows(2)
+                .all(|pair| (1..=3).contains(&(pair[1]-pair[0]).mul(sign)))
         })
+
     }).count();
 
     println!("Part 1: {}", part_1);
     println!("Part 2: {}", part_2);
 
 
-}
-
-
-fn is_report_valid(report: &[isize]) -> bool {
-    let is_inc = report[0] < report[2];
-
-    return report.windows(2).all(|pair|{
-        let diff: isize = (pair[1] - pair[0]) * (if is_inc { 1 } else { -1 });
-        (1..=3).contains(&diff)
-    });
 }
