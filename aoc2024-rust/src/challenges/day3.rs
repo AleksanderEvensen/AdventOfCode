2b5b67564bbc63bb1417291ce0f594f5222a202b
Aleksander Evensen <ahse03@gmail.com>
2024-12-03 23:28:35 +0100

    Rust 2024 one regex does it all

--- aoc2024-rust/src/challenges/day3.rs
+++ aoc2024-rust/src/challenges/day3.rs
@@ -1,20 +1,20 @@
 use regex::Regex;
 
 pub fn solve(input: String) {
-    let rxp = Regex::new(r"(do\(\)|don't\(\)|mul\(\d+,\d+\))").expect("Invalid regex");
-    let rxp_num = Regex::new(r"mul\((\d+),(\d+)\)").expect("Invalid regex");
+    let rxp = Regex::new(r"(do\(\)|don't\(\)|mul\((?<a>\d+),(?<b>\d+)\))").expect("Invalid regex");
 
     let mut disabled = false;
-    let (part_1, part_2) = rxp.find_iter(&input).fold((0,0), |(mut part_1, mut part_2), op| {
-        if op.as_str().starts_with("mul") {
-            
-            let (_, [a,b]) = rxp_num.captures(op.as_str()).unwrap().extract();
-            let value = a.parse::<i32>().unwrap() * b.parse::<i32>().unwrap();
+    let (part_1, part_2) = rxp.captures_iter(&input).fold((0,0), |(mut part_1, mut part_2), caps| {
+        let op = caps.get(0).unwrap().as_str();
+        if op.starts_with("mul") {
+            let a = caps.name("a").unwrap().as_str().parse::<i32>().unwrap();            
+            let b = caps.name("b").unwrap().as_str().parse::<i32>().unwrap();
 
+            let value = a*b;
             part_1 += value;
             part_2 += value * if disabled { 0 } else { 1 };
             
-        } else if op.as_str().starts_with("don't") {
+        } else if op.starts_with("don't") {
             disabled = true;
         } else {
             disabled = false;
